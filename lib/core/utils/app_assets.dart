8f81d69462d2a4e104432a078827502ff6810da6
gelkomyy <string.cf@gmail.com>
2024-08-21 02:43:37 +0300

    Add new assets and custom text field widget
    - Introduced a new SVG for the home header shape.
    - Added a search icon SVG for use in the custom text field.
    - Included a white heart PNG asset.
    - Updated the app assets Dart file to reference the new assets.
    - Created a custom text field widget that utilizes the new search icon SVG and includes styling adjustments for better integration within the app's design.

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,8 +1,20 @@
 class Assets {
   Assets._();
   
+  /// Assets for assetsImagesHomeHeaderShapeSvg
+  /// assets/images/home_header_shape_svg.svg
+  static const String assetsImagesHomeHeaderShapeSvg = "assets/images/home_header_shape_svg.svg";
+
   /// Assets for assetsImagesIntroBackground
   /// assets/images/intro_background.jpg
   static const String assetsImagesIntroBackground = "assets/images/intro_background.jpg";
+
+  /// Assets for assetsImagesSearchIconSvg
+  /// assets/images/search_icon_svg.svg
+  static const String assetsImagesSearchIconSvg = "assets/images/search_icon_svg.svg";
+
+  /// Assets for assetsImagesWhiteHeart
+  /// assets/images/white_heart.png
+  static const String assetsImagesWhiteHeart = "assets/images/white_heart.png";
 }
 
---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,57 @@
+import 'package:fazakir/core/utils/app_assets.dart';
+import 'package:fazakir/core/utils/app_colors.dart';
+import 'package:fazakir/core/utils/app_font_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hintText});
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: AppColors.primaryColor,
+      style: AppFontStyles.styleRegular11(context).copyWith(
+        fontFamily: 'Almarai',
+      ),
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 8,
+        ),
+        hintText: hintText,
+        hintStyle: AppFontStyles.styleRegular11(context).copyWith(
+          color: AppColors.greyColor,
+          fontFamily: 'Almarai',
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        suffixIcon: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          child: SvgPicture.asset(
+            Assets.assetsImagesSearchIconSvg,
+            width: 16,
+            height: 16,
+          ),
+        ),
+        suffixIconConstraints: const BoxConstraints(
+          minHeight: 12,
+          minWidth: 12,
+        ),
+        border: getBorder(),
+        enabledBorder: getBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder getBorder() {
+    return const OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.all(
+        Radius.circular(5),
+      ),
+    );
+  }
+}
