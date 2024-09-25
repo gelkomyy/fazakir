f7ed07790cb3f4bbe767701f433a343abff46bcc
gelkomyy <string.cf@gmail.com>
2024-09-26 01:56:24 +0300

    Refactor AhadithViewBody widget to use a FutureBuilder
    The AhadithViewBody widget has been refactored to use a FutureBuilder to asynchronously fetch the ahadith data. This improves the performance and user experience of the app. The commit also includes some minor code cleanup and error handling.

--- lib/Features/ahadith/presentation/views/widgets/ahadith_view_body.dart
+++ lib/Features/ahadith/presentation/views/widgets/ahadith_view_body.dart
@@ -1,7 +1,11 @@
+import 'dart:isolate';
+
 import 'package:fazakir/Features/ahadith/domain/entities/hadith_entity.dart';
 import 'package:fazakir/Features/ahadith/presentation/views/widgets/container_hadith_item.dart';
 import 'package:fazakir/core/extensions/manage_hadiths_extensions.dart';
+import 'package:fazakir/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:hadith/classes.dart';
 import 'package:hadith/hadith.dart';
 
 class AhadithViewBody extends StatelessWidget {
@@ -13,34 +17,61 @@ class AhadithViewBody extends StatelessWidget {
   final String bookName;
   @override
   Widget build(BuildContext context) {
-    final ahadith = getHadiths(
-        ManageHadithsExtensions.getCollectionByName(bookName),
-        sectionOfBookHadithNumber);
-    return Padding(
-      padding: const EdgeInsets.all(18),
-      child: ListView.builder(
-        clipBehavior: Clip.none,
-        itemCount: ahadith.length,
-        itemBuilder: (BuildContext context, int index) {
+    return FutureBuilder(
+      future: Isolate.run(
+        () => getHadiths(ManageHadithsExtensions.getCollectionByName(bookName),
+            sectionOfBookHadithNumber),
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<Hadith> ahadith = snapshot.data as List<Hadith>;
           return Padding(
-            padding: EdgeInsetsDirectional.only(
-                bottom: index + 1 == ahadith.length ? 0 : 24),
-            child: ContainerHadithItem(
-              hadithEntity: HadithEntity(
-                hadith: parseHadith(ahadith[index].hadith[1].body),
-                bookName: bookName,
-                sectionOfBookHadith: getBook(
-                        ManageHadithsExtensions.getCollectionByName(bookName),
-                        int.parse(ahadith[index].bookNumber))
-                    .book[1]
-                    .name,
-                hadithNumber: ahadith[index].hadithNumber,
-                grades: ahadith[index].hadith[1].grades,
-              ),
+            padding: const EdgeInsets.all(18),
+            child: ListView.builder(
+              clipBehavior: Clip.none,
+              itemCount: ahadith.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsetsDirectional.only(
+                      bottom: index + 1 == ahadith.length ? 0 : 24),
+                  child: ContainerHadithItem(
+                    hadithEntity: HadithEntity(
+                      hadith: parseHadith(ahadith[index].hadith[1].body),
+                      bookName: bookName,
+                      sectionOfBookHadith: getBook(
+                              ManageHadithsExtensions.getCollectionByName(
+                                  bookName),
+                              int.parse(ahadith[index].bookNumber))
+                          .book[1]
+                          .name,
+                      hadithNumber: ahadith[index].hadithNumber,
+                      grades: ahadith[index].hadith[1].grades,
+                    ),
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator.adaptive(
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.primaryColor),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text(snapshot.error.toString()),
+          );
+        } else if (!snapshot.hasData) {
+          return const Center(
+            child: Text('No Data'),
+          );
+        } else {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+      },
     );
   }
 
