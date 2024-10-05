38b38de147009258544bf508c847c8f7bb731cd3
gelkomyy <string.cf@gmail.com>
2024-10-05 07:47:58 +0300

    Refactor settings view body layout and add spacing between elements
    The commit refactors the layout of the settings view body by adding a `SingleChildScrollView` and spacing between elements. It also adds a `SizedBox` between the settings list and the Elkomy logo row, and between the share button and the bottom of the screen.

--- lib/Features/settings/presentation/widgets/notify_duration_dialog_content.dart
+++ lib/Features/settings/presentation/widgets/notify_duration_dialog_content.dart
@@ -148,6 +148,7 @@ class _NotifyDurationDialogContentState
             ),
           ],
         ),
+        const SizedBox(height: 12),
       ],
     );
   }
--- lib/Features/settings/presentation/widgets/settings_view_body.dart
+++ lib/Features/settings/presentation/widgets/settings_view_body.dart
@@ -12,132 +12,137 @@ class SettingsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ...getSettingsList(context).map((e) => e.buildList(context)),
-          const SizedBox(
-            height: 6,
-          ),
-          Container(
-            width: double.infinity,
-            height: 145,
-            decoration: ShapeDecoration(
-              color: const Color(0x51BBBBBB),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ...getSettingsList(context).map((e) => e.buildList(context)),
+            const SizedBox(
+              height: 6,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      Assets.assetsImagesElkomyLogo,
-                      width: 42,
-                      height: 42,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Text(
-                      'برمجة وتطوير',
-                      style: AppFontStyles.styleRegular14(context).copyWith(
-                        color: AppColors.primaryColor,
+            Container(
+              width: double.infinity,
+              height: 145,
+              decoration: ShapeDecoration(
+                color: const Color(0x51BBBBBB),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        Assets.assetsImagesElkomyLogo,
+                        width: 42,
+                        height: 42,
+                        fit: BoxFit.cover,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      'م: جاسر الكومى',
-                      style: AppFontStyles.styleBold14(context),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        openLink('https://www.linkedin.com/in/gelkomy/');
-                      },
-                      child: SvgPicture.asset(
-                        Assets.assetsImagesLinkedinIconSvg,
-                        width: 18,
-                        height: 18,
+                      const SizedBox(
+                        height: 6,
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      Assets.assetsImagesElkomyLogo,
-                      width: 42,
-                      height: 42,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Text(
-                      'مصمم واجهات',
-                      style: AppFontStyles.styleRegular14(context).copyWith(
-                        color: AppColors.primaryColor,
+                      Text(
+                        'برمجة وتطوير',
+                        style: AppFontStyles.styleRegular14(context).copyWith(
+                          color: AppColors.primaryColor,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      'م: زياد محمد',
-                      style: AppFontStyles.styleBold14(context),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        openLink('https://www.linkedin.com/in/gelkomy/');
-                      },
-                      child: SvgPicture.asset(
-                        Assets.assetsImagesLinkedinIconSvg,
-                        width: 18,
-                        height: 18,
+                      const SizedBox(
+                        height: 4,
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          CustomTextButton(
-            onPressed: () {},
-            padding: const EdgeInsets.all(14),
-            text: '',
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SvgPicture.asset(Assets.assetsImagesStarIconSvg),
-                Text(
-                  'شارك التجربة مع الاصدقاء',
-                  style: AppFontStyles.styleBold14(context).copyWith(
-                    color: Colors.white,
+                      Text(
+                        'م: جاسر الكومى',
+                        style: AppFontStyles.styleBold14(context),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          openLink('https://www.linkedin.com/in/gelkomy/');
+                        },
+                        child: SvgPicture.asset(
+                          Assets.assetsImagesLinkedinIconSvg,
+                          width: 18,
+                          height: 18,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                SvgPicture.asset(Assets.assetsImagesStarIconSvg),
-              ],
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        Assets.assetsImagesElkomyLogo,
+                        width: 42,
+                        height: 42,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        'مصمم واجهات',
+                        style: AppFontStyles.styleRegular14(context).copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'م: زياد محمد',
+                        style: AppFontStyles.styleBold14(context),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          openLink('https://www.linkedin.com/in/gelkomy/');
+                        },
+                        child: SvgPicture.asset(
+                          Assets.assetsImagesLinkedinIconSvg,
+                          width: 18,
+                          height: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 32,
+            ),
+            CustomTextButton(
+              onPressed: () {},
+              padding: const EdgeInsets.all(14),
+              text: '',
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SvgPicture.asset(Assets.assetsImagesStarIconSvg),
+                  Text(
+                    'شارك التجربة مع الاصدقاء',
+                    style: AppFontStyles.styleBold14(context).copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SvgPicture.asset(Assets.assetsImagesStarIconSvg),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 90,
+            ),
+          ],
+        ),
       ),
     );
   }
