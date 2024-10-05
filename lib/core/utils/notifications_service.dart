3fb92928549e38a2bfb113cfff6b480adc794d7a
gelkomyy <string.cf@gmail.com>
2024-10-05 03:17:37 +0300

    feat: Add Android intent and notification channel to notifications service
    This commit adds the `android_intent_plus` and `android_notification_channel` packages to the `NotificationService` class in order to request notification permissions and auto-start permission. It also updates the `showAzkarNotification` and `scheduleAzkarNotification` methods to use the new notification channel and adds a new `callbackShowAzkarNotification` method to handle the periodic showing of notifications. Finally, the commit updates the `pubspec.yaml` file to include the new dependencies.

--- lib/core/utils/notifications_service.dart
+++ lib/core/utils/notifications_service.dart
@@ -1,5 +1,9 @@
 import 'dart:async';
+import 'dart:developer' as dev;
+import 'dart:math';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:android_intent_plus/flag.dart';
 import 'package:fazakir/Features/azkar/data/repos/azkar_repo_impl.dart';
 import 'package:fazakir/Features/azkar/domain/entities/azkar_category_entity.dart';
 import 'package:fazakir/core/utils/func/get_it_setup.dart';
@@ -16,6 +20,19 @@ class NotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
+  static final AndroidNotificationChannel channel = AndroidNotificationChannel(
+    Random.secure().nextInt(100000).toString(),
+    'High Importance Notifications',
+    importance: Importance.high,
+  );
+
+  static Future<void> requestNotificationsPermission() async {
+    await _notificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+  }
+
   static Future<void> initNotification() async {
     tz.initializeTimeZones();
     const AndroidInitializationSettings androidInitializationSettings =
@@ -31,58 +48,74 @@ class NotificationService {
     );
 
     await _notificationsPlugin.initialize(initializationSettings);
+
+    await requestNotificationsPermission();
+    await requestAutoStartPermission();
   }
 
   // Show the notification for the Azkar content
+  @pragma('vm:entry-point')
   static Future<void> showAzkarNotification(
       AzkarCategoryEntity azkarCategory) async {
     if (!sendNotify) return;
-    const AndroidNotificationDetails androidDetails =
-        AndroidNotificationDetails(
-      'azkar_channel', // Channel ID
-      'Azkar Notifications', // Channel name
+
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      channel.id,
+      channel.name,
       channelDescription: 'Reminder for Azkar',
       importance: Importance.max,
       priority: Priority.high,
+      ticker: 'ticker',
     );
 
-    const DarwinNotificationDetails iosDetails = DarwinNotificationDetails();
+    const DarwinNotificationDetails iosDetails = DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
 
-    const NotificationDetails platformDetails =
-        NotificationDetails(android: androidDetails, iOS: iosDetails);
-    final int uniqueId = DateTime.now()
-        .millisecondsSinceEpoch
-        .remainder(100000); // Generate a unique ID using timestamp
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
     await _notificationsPlugin.show(
-      uniqueId, // Notification ID
+      330, // Notification ID
       azkarCategory
           .azkar.first.text, // Content text (limited to 410 characters)
       azkarCategory.category, // Category as subtitle
-      platformDetails,
+      notificationDetails,
     );
   }
 
   // Schedule Azkar notification every hour
+  @pragma('vm:entry-point')
   static Future<void> scheduleAzkarNotification() async {
     if (!sendNotify) return;
-    final int uniqueId = DateTime.now()
-        .millisecondsSinceEpoch
-        .remainder(100000); // Generate a unique ID using timestamp
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      channelDescription: 'Reminder for Azkar',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    const DarwinNotificationDetails iosDetails = DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iosDetails,
+    );
     await _notificationsPlugin.zonedSchedule(
-      uniqueId,
+      330,
       'Azkar Reminder',
       'Fetching Azkar...',
       tz.TZDateTime.now(tz.local).add(notificationDuration),
-      const NotificationDetails(
-        android: AndroidNotificationDetails(
-          'azkar_channel',
-          'Azkar Notifications',
-          channelDescription: 'Reminder for Azkar',
-          importance: Importance.max,
-          priority: Priority.high,
-        ),
-        iOS: DarwinNotificationDetails(),
-      ),
+      notificationDetails,
       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime,
@@ -90,21 +123,22 @@ class NotificationService {
     );
 
     // Fetch the azkar in the background using a callback
-    Timer.periodic(notificationDuration, (Timer timer) async {
-      AzkarCategoryEntity randomAzkar = await fetchRandomAzkarWithLimit();
-      await showAzkarNotification(randomAzkar); // Show the notification
-    });
+    Timer.periodic(
+      notificationDuration,
+      (Timer timer) async => await callbackShowAzkarNotification(),
+    );
   }
 
+  @pragma('vm:entry-point')
   static Future<void> scheduleNotification() async {
     if (!sendNotify) return;
-    final int alarmId = DateTime.now().millisecondsSinceEpoch.remainder(100000);
-    await AndroidAlarmManager.periodic(
-      notificationDuration, alarmId, // Unique ID for the alarm
-      () async {
-        AzkarCategoryEntity azkar = await fetchRandomAzkarWithLimit();
-        await showAzkarNotification(azkar);
-      }, // The callback function
+    await AndroidAlarmManager.cancel(330);
+
+    await _notificationsPlugin.cancelAll();
+
+    var result = await AndroidAlarmManager.periodic(
+      notificationDuration, 330, // Unique ID for the alarm
+      callbackShowAzkarNotification, // The callback function
       wakeup: true, // Wake up the device if it is asleep
       exact: true, // Ensure it runs exactly every hour
       allowWhileIdle:
@@ -112,21 +146,41 @@ class NotificationService {
       rescheduleOnReboot:
           true, // Reschedule the alarm if the device is rebooted
     );
+    if (!result) {
+      dev.log('AndroidAlarmManager.periodic: false result');
+      await scheduleAzkarNotification();
+      dev.log('The scheduleAzkarNotification done.');
+    }
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> callbackShowAzkarNotification() async {
+    AzkarCategoryEntity randomAzkar = await fetchRandomAzkarWithLimit();
+    await showAzkarNotification(randomAzkar);
   }
 
   // Update the notification duration
-  void updateNotificationDuration(Duration newDuration) {
+  static void updateNotificationDuration(Duration newDuration) {
     notificationDuration = newDuration;
     // Reschedule the notification with the new duration
     scheduleNotification();
   }
 
 // Set the sendNotify flag
-  void setSendNotify(bool value) {
+  static void setSendNotify(bool value) {
     sendNotify = value;
     if (sendNotify) {
       scheduleNotification(); // Reschedule if notifications are enabled
     }
+  }
+
+  static Future<void> requestAutoStartPermission() async {
+    const intent = AndroidIntent(
+      action: 'android.settings.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS',
+      data: 'package:com.fazakir.elkomy',
+      flags: <int>[Flag.FLAG_ACTIVITY_NEW_TASK],
+    );
+    await intent.launch();
   }
 }
 
@@ -136,7 +190,7 @@ Future<AzkarCategoryEntity> fetchRandomAzkarWithLimit() async {
   while (!validLength) {
     azkarCategory = await getIt<AzkarRepoImpl>()
         .getRandomAzkarCategory(); // Fetch random AzkarCategoryEntity
-    if (azkarCategory.azkar.first.text.length <= 410) {
+    if (azkarCategory.azkar.first.text.length <= 120) {
       validLength = true; // Found contentText with valid length
     }
   }
