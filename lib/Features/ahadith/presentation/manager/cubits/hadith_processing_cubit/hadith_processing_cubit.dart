4b6d923194cd7823be5c6b98561cf2be1d57b5c1
gelkomyy <string.cf@gmail.com>
2024-10-22 02:33:26 +0300

    Refactor hadith processing to use isolates
    This commit refactors the hadith processing logic to utilize
    isolates for background processing. The previous synchronous
    approach has been replaced with asynchronous functions that
    spawn isolates, improving performance and responsiveness.
    
    Key changes include:
    - Introduction of `processHadithsInBackground` for heavy
      computation tasks.
    - Removal of redundant methods and comments.
    - Enhanced error handling during hadith fetching.

--- lib/Features/ahadith/presentation/manager/cubits/hadith_processing_cubit/hadith_processing_cubit.dart
+++ lib/Features/ahadith/presentation/manager/cubits/hadith_processing_cubit/hadith_processing_cubit.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'dart:isolate';
-
 import 'package:fazakir/Features/ahadith/domain/entities/hadith_entity.dart';
 import 'package:fazakir/Features/ahadith/presentation/views/widgets/ahadith_view_body.dart';
 import 'package:fazakir/Features/azkar/data/repos/azkar_repo_impl.dart';
@@ -18,7 +17,6 @@ part 'hadith_processing_state.dart';
 class HadithProcessingCubit extends Cubit<HadithProcessingState> {
   HadithProcessingCubit() : super(HadithProcessingInitial()) {
     _loadHadithFromLocal();
-    // Load data from Local on cubit creation
   }
 
   List<HadithEntity> _allAhadith = [];
@@ -52,90 +50,22 @@ class HadithProcessingCubit extends Cubit<HadithProcessingState> {
     safeEmit(HadithProcessingLoading());
 
     if (_allAhadith.isNotEmpty) {
-      //  no need to process
+      // No need to process
       safeEmit(HadithProcessingLoaded(_allAhadith));
       return;
     }
+
     try {
-      List<List<HadithEntity>> results = await processTheHadiths();
-      _allAhadith = results.expand((list) => list).toList();
+      // Process hadiths in the background using compute
+      List<HadithEntity> processedHadiths =
+          await compute(processHadithsInBackground, null);
+
+      _allAhadith = processedHadiths;
       await _saveHadithToLocal();
       safeEmit(HadithProcessingLoaded(_allAhadith));
     } catch (e) {
       safeEmit(HadithProcessingError('Failed to process hadiths.'));
     }
-  }
-
-  Future<List<List<HadithEntity>>> processTheHadiths() async {
-    final a6BooksOfHadiths = ManageHadithsExtensions.getDisplayNames();
-
-    List<Future<List<HadithEntity>>> hadithFutures = [];
-    for (String bookName in a6BooksOfHadiths) {
-      hadithFutures.add(_fetchHadithForBook(bookName));
-    }
-
-    final results = await Future.wait(hadithFutures);
-    return results;
-  }
-
-  Future<List<HadithEntity>> _fetchHadithForBook(String bookName) async {
-    final ReceivePort receivePort = ReceivePort();
-    await Isolate.spawn(_isolateProcess, {
-      'sendPort': receivePort.sendPort,
-      'bookName': bookName,
-    });
-
-    final List<dynamic> result = await receivePort.first;
-    return (result).map((item) => HadithEntity.fromJson(item)).toList();
-  }
-
-  static void _isolateProcess(Map<String, dynamic> message) async {
-    final SendPort sendPort = message['sendPort'];
-    final String bookName = message['bookName'];
-
-    final sectionsOfBookHadith =
-        getBooks(ManageHadithsExtensions.getCollectionByName(bookName));
-    List<Map<String, dynamic>> processedHadiths = [];
-
-    for (var section in sectionsOfBookHadith) {
-      final sectionNumber = _getSectionNumber(bookName, section.bookNumber);
-      List<Hadith> ahadith = [];
-      try {
-        ahadith = getHadiths(
-            ManageHadithsExtensions.getCollectionByName(bookName),
-            sectionNumber);
-      } catch (e) {
-        log('Error: $e at --- $bookName:$sectionNumber---');
-      }
-
-      for (Hadith hadith in ahadith) {
-        processedHadiths.add({
-          'hadith': parseHadith(hadith.hadith[1].body),
-          'bookName': bookName,
-          'sectionOfBookHadith': getBook(
-                  ManageHadithsExtensions.getCollectionByName(bookName),
-                  int.parse(hadith.bookNumber))
-              .book[1]
-              .name,
-          'hadithNumber': hadith.hadithNumber,
-          'grades': hadith.hadith[1].grades,
-        });
-      }
-    }
-
-    sendPort.send(processedHadiths);
-  }
-
-  static int _getSectionNumber(String bookName, String bookNumber) {
-    if (bookNumber == 'introduction') bookNumber = '1';
-    bookNumber = bookNumber.replaceAll(RegExp(r'[^0-9]'), '');
-    if (bookName == 'سنن ابن ماجه' || bookName == 'صحيح مسلم') {
-      final parsedNumber = int.tryParse(bookNumber);
-
-      return parsedNumber ?? 1;
-    }
-
-    return int.parse(bookNumber);
   }
 
   Future<SearchResult> filterHadiths(String searchHadith) async {
@@ -190,4 +120,86 @@ class HadithProcessingCubit extends Cubit<HadithProcessingState> {
     return SearchResult(
         exactMatches: exactMatches, relatedMatches: relatedMatches);
   }
+}
+
+// Top-level function for processing hadiths in background
+Future<List<HadithEntity>> processHadithsInBackground(dynamic _) async {
+  // Process the hadiths (this is the heavy computation task)
+  List<List<HadithEntity>> results = await processTheHadiths();
+
+  // Flatten the list of lists into a single list and return it
+  return results.expand((list) => list).toList();
+}
+
+// Top-level function to handle the hadiths processing with an isolate
+Future<List<List<HadithEntity>>> processTheHadiths() async {
+  final a6BooksOfHadiths = ManageHadithsExtensions.getDisplayNames();
+
+  List<Future<List<HadithEntity>>> hadithFutures = [];
+  for (String bookName in a6BooksOfHadiths) {
+    hadithFutures.add(_fetchHadithForBook(bookName));
+  }
+
+  final results = await Future.wait(hadithFutures);
+  return results;
+}
+
+Future<List<HadithEntity>> _fetchHadithForBook(String bookName) async {
+  final ReceivePort receivePort = ReceivePort();
+  await Isolate.spawn(_isolateProcess, {
+    'sendPort': receivePort.sendPort,
+    'bookName': bookName,
+  });
+
+  final List<dynamic> result = await receivePort.first;
+  return result.map((item) => HadithEntity.fromJson(item)).toList();
+}
+
+// Top-level isolate process function
+void _isolateProcess(Map<String, dynamic> message) async {
+  final SendPort sendPort = message['sendPort'];
+  final String bookName = message['bookName'];
+
+  final sectionsOfBookHadith =
+      getBooks(ManageHadithsExtensions.getCollectionByName(bookName));
+  List<Map<String, dynamic>> processedHadiths = [];
+
+  for (var section in sectionsOfBookHadith) {
+    final sectionNumber = _getSectionNumber(bookName, section.bookNumber);
+    List<Hadith> ahadith = [];
+    try {
+      ahadith = getHadiths(
+          ManageHadithsExtensions.getCollectionByName(bookName), sectionNumber);
+    } catch (e) {
+      log('Error: $e at --- $bookName:$sectionNumber---');
+    }
+
+    for (Hadith hadith in ahadith) {
+      processedHadiths.add({
+        'hadith': parseHadith(hadith.hadith[1].body),
+        'bookName': bookName,
+        'sectionOfBookHadith': getBook(
+                ManageHadithsExtensions.getCollectionByName(bookName),
+                int.parse(hadith.bookNumber))
+            .book[1]
+            .name,
+        'hadithNumber': hadith.hadithNumber,
+        'grades': hadith.hadith[1].grades,
+      });
+    }
+  }
+
+  sendPort.send(processedHadiths);
+}
+
+// Utility function to get section number
+int _getSectionNumber(String bookName, String bookNumber) {
+  if (bookNumber == 'introduction') bookNumber = '1';
+  bookNumber = bookNumber.replaceAll(RegExp(r'[^0-9]'), '');
+  if (bookName == 'سنن ابن ماجه' || bookName == 'صحيح مسلم') {
+    final parsedNumber = int.tryParse(bookNumber);
+    return parsedNumber ?? 1;
+  }
+
+  return int.parse(bookNumber);
 }
